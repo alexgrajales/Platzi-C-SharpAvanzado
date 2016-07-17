0ab1b14b7b4901b9014d9df20f193ad8fb9a54b6
alexander grajales <alex-12-04@hotmail.com>
2016-07-17 11:21:01 -0500

    Se crea libreria para identificar que en una carpeta hay archivos o directorios

---
+++ execpciones/DllDirectorios/FileHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace librerias
+{
+    public class FileHelper
+    {
+        public List<FileObjectInfo> GetFileSystemObjects(string path)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+            var listaInfo = di.EnumerateFileSystemInfos();
+            var fileObject = new List<FileObjectInfo>();
+            foreach (var fso in listaInfo)
+            {
+                FileObjectType tipo = FileObjectType.Directory;
+                if (fso.Attributes == FileAttributes.Archive)
+                {
+                    tipo = FileObjectType.File;
+                }
+                fileObject.Add(new FileObjectInfo() { Path = fso.FullName, Name = fso.Name, FileType = tipo });
+
+            }
+
+            return fileObject;
+        }
+    }
+}
---
+++ execpciones/librerias/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace librerias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileHelper fh = new FileHelper();
+            var respuesta = fh.GetFileSystemObjects("./");
+            foreach (var item in respuesta)
+            {
+                Console.WriteLine($"Name:{item.Name}-Tipo:{item.FileType}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
